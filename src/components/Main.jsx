5e4a14b4f6e3e27fc46660edfb2338e35115b54e
Musau Wambua <musau@Musaus-MacBook-Air.local>
2022-11-03 18:07:56 -0500

    Leaflet map of Lawrence Country Club + markers at first 9 holes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,12 @@
 import React, {useState} from 'react'
 import { Icon } from '@iconify/react';
 import map from '../Images/map.jpg'
-
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import { MapContainer } from 'react-leaflet';
+// import { TileLayer } from 'leaflet';
+// import { Marker } from 'react-leaflet';
+// import { Popup } from 'react-leaflet';
+// import { useMap } from 'react-leaflet';
 const Main = () => {
 
   const [count, setCount] = useState(1);
@@ -38,7 +43,64 @@ const Main = () => {
 
       
       <div className='bg-[#3E3B3B] pl-4 py-3 rounded-l-xl'>
-         <iframe title="map" className='map rounded-l-xl' width="500" height="780" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-95.26860415935516%2C38.976263117537805%2C-95.26184499263765%2C38.97992040605664&amp;layer=mapnik&amp;marker=38.978091785409354%2C-95.2652245759964"></iframe>
+         {/* <iframe title="map" className='map rounded-l-xl' width="500" height="780" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-95.26860415935516%2C38.976263117537805%2C-95.26184499263765%2C38.97992040605664&amp;layer=mapnik&amp;marker=38.978091785409354%2C-95.2652245759964"></iframe> */}
+         <MapContainer center = {[38.979, -95.266]} zoom = {16}>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> 
+              contributors'
+            />
+            <Marker position={[38.9776672, -95.2634994]}>
+              <Popup>
+                Hole 1
+              </Popup>
+            </Marker>
+            <Marker position={[38.9776672, -95.2634994]}>
+              <Popup>
+                Hole 1
+              </Popup>
+            </Marker>
+            <Marker position={[38.9786234, -95.2675139]}>
+              <Popup>
+                Hole 2
+              </Popup>
+            </Marker>
+            <Marker position={[38.9788485, -95.2688737]}>
+              <Popup>
+                Hole 3
+              </Popup>
+            </Marker>
+            <Marker position={[38.9829038, -95.2683226]}>
+              <Popup>
+                Hole 4
+              </Popup>
+            </Marker>
+            <Marker position={[38.9788209, -95.2676372]}>
+              <Popup>
+                Hole 5
+              </Popup>
+            </Marker>
+            <Marker position={[38.9819392, -95.2653599]}>
+              <Popup>
+                Hole 6
+              </Popup>
+            </Marker>
+            <Marker position={[38.9780217, -95.2634658]}>
+              <Popup>
+                Hole 7
+              </Popup>
+            </Marker>
+            <Marker position={[38.9787367, -95.2607026]}>
+              <Popup>
+                Hole 8
+              </Popup>
+            </Marker>
+            <Marker position={[38.9752793, -95.2623357]}>
+              <Popup>
+                Hole 9
+              </Popup>
+            </Marker>
+          </MapContainer>         
       </div>
     </div>
 
